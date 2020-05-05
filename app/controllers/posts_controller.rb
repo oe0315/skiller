5a93d32615cc02d0f43125f23db4ecff20665273
oe0315 <oeoe0315@gmail.com>
2020-05-05 04:23:05 +0000

    [add]post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
 
 	def index
-		
+		@post = Post.new
+		@posts = Post.all
 	end
 
 	def create
-		
+		@post = Post.new(post_params)
+		url = params[:post][:youtube_url]
+        url = url.last(11)
+        @post.youtube_url = url
+		@post.save
+		redirect_to posts_path
 	end
 
 	def edit
@@ -19,4 +25,9 @@ class PostsController < ApplicationController
 	def destroy
 		
 	end
+
+  private
+    def post_params
+    	params.require(:post).permit(:body, :youtube_url)
+    end
 end
