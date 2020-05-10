f73355bc56d8f0279b5b6072f756a7d57b8c8fb1
oe0315 <oeoe0315@gmail.com>
2020-05-10 06:48:35 +0000

    通知

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,9 @@
 class NotificationsController < ApplicationController
+
+	def index
+		@notifications = current_user.passive_notifications
+		@notifications.where(checked: false).each do |notification|
+			notification.update_attributes(checked: true)
+		end
+	end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,9 @@
 class RelationshipsController < ApplicationController
 
 	def follow
-		@user = params[:id]
-        current_user.follow(params[:id])
-        @user.create_notification_follow(current_user)
+        @follow = current_user.follow(params[:id])
+        @user = User.find(@follow.follower_id)
+        @user.create_notification_follow!(current_user)
         redirect_back(fallback_location: root_url)
 	end
 
