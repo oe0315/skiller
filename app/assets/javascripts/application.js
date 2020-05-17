4f79aa904ba1213710be57e33633ca1a8ce0da0b
oe0315 <oeoe0315@gmail.com>
2020-05-17 16:44:49 +0000

    細かい訂正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
-// require turbolinks
-//= require_tree
 //= require popper
 //= require jquery3
+//= require rails-ujs
+// require turbolinks
+//= require_tree .
 //= require bootstrap-sprockets
 /*!
  * jQuery Raty - A Star Rating Plugin
