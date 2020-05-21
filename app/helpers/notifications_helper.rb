70bec975df546baa6b24d165880f7d8bf26503f9
oe0315 <oeoe0315@gmail.com>
2020-05-21 11:32:36 +0000

    レスポンシブ

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,6 @@
 module NotificationsHelper
 
     def unchecked_notifications
-        @notifications = current_user.passive_notifications.where(checked: false)
+        current_user.passive_notifications.where(checked: false)
     end
 end
