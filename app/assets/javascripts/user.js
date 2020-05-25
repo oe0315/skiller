064282c3f165ce003614f9c500a3b4fd06c919c3
oe0315 <oeoe0315@gmail.com>
2020-05-25 22:55:38 +0900

    [fix]インクリメンタルサーチの画像表示の訂正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -15,7 +15,7 @@ $(document).on('turbolinks:load', function(){
         $('#result').find('tr').remove();
             $(data).each(function(i, user){
                 $('#result').append(
-                    '<tr>' + '<td>' + '<a href="/users/' + user.id + '">' + '<img src ="' + user.profile_image.url + '"width=64 height=64 class="rounded-circle" onerror="this.onerror = null; this.src=' + "'/no-image-icon.jpg'" + '">' + '</a>' + '</td>'
+                    '<tr>' + '<td>' + '<a href="/users/' + user.id + '">' + '<img src ="' + user.profile_image.url + '"width=64 height=64 class="rounded-circle" onerror="this.onerror = null; this.src=' + "'no-image-icon.jpg'" + '">' + '</a>' + '</td>'
                     + '<td>' + user.nickname + '</td>'
                     + '<td>' + user.skill + '</td>'
                     + '<td>' + user.second_skill + '</td>'
