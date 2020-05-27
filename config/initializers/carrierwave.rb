5504078b98be93a638aa512287c4226d249c312b
oe0315 <oeoe0315@gmail.com>
2020-05-27 21:45:15 +0900

    s3訂正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,24 @@
-unless Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['aws_access_key_id'],
-        aws_secret_access_key: ENV['aws_secret_access_key'],
-        region: 'ap-northeast-1'
-      }
-      config.fog_directory  = 'skilleroe'
-      config.cache_storage = :fog
-    end
+
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'skilleroe'
+    config.asset_host = 'https://skilleroe.s3-ap-northeast-1.amazonaws.com/'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
+      region: 'ap-northeast-1',
+      # path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
